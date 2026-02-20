6b0c3e2841caeeda86d1086fe729ab68b9f07cf2
Ingrid <caderno.da.ingrid@gmail.com>
2026-02-20 13:04:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const dadosTreinos = {
     "titulo": "Membros Superiores",
     "subtitulo": "Conjugado",
     "exercicios": [
-      {"nome": "Supino Reto", "execucao": "4 séries • 8/12 reps", "peso": "12.5/20", "tec": "PC/CC", "obs": "Pirâmide Crescente (aumente o peso a cada série). | Cadência controlada (3 seg. na descida)." },
-      {"nome": "Remada Curvada ou Baixa", "execucao": "4 séries • 8/12 reps", "peso": "15/20", "tec": "RP", "obs": "Rest-Pause na última série (faça 6 reps, descanse 15s, faça o máximo que aguentar) | __" },
+      {"nome": "Supino Reto com barrra", "execucao": "4 séries • 8/12 reps", "peso": "12.5/20", "tec": "PC/CC", "obs": "Pirâmide Crescente (aumente o peso a cada série). | Cadência controlada (3 seg. na descida)." },
+      {"nome": "Remada Baixa", "execucao": "4 séries • 8/12 reps", "peso": "15/20", "tec": "RP", "obs": "Rest-Pause na última série (faça 6 reps, descanse 15s, faça o máximo que aguentar) | __" },
       {"nome": "Desenvolvimento (Ombros)", "execucao": "3 séries • 10/12 reps", "peso": "5/7", "tec": "Bi-set", "obs": "Fazer 30s de polichinelos" },
-      {"nome": "Puxada Alta (Lat Pulldown)", "execucao": "3 séries • 10/12 reps", "peso": "19/33", "tec": "CM/DS", "obs": "Foco na contração máxima das costas. | Drop-set na última série (reduz o peso em 30% e faz até a falha)" },
-      {"nome": "Rosca Direta + Tríceps Corda", "execucao": "3 séries • 10/15 reps", "peso": "5/5", "tec": "Super-Série", "obs": "Um exercício seguido do outro sem descanso." },
+      {"nome": "Puxada Alta Unilateral", "execucao": "3 séries • 10/12 reps", "peso": "19/33", "tec": "CM/DS", "obs": "Foco na contração máxima das costas. | Drop-set na última série (reduz o peso em 30% e faz até a falha)" },
+      {"nome": "Rosca Cabo + Tríceps Unilateral", "execucao": "3 séries • 12 reps", "peso": "5/5", "tec": "Super-Série", "obs": "Um exercício seguido do outro sem descanso." },
       {"nome": "Prancha Abdominal", "execucao": "3 séries • 1 min", "peso": " ", "tec": " ", "obs": " " }
     ]
   },
@@ -27,12 +27,12 @@ const dadosTreinos = {
     "titulo": "Full Body",
     "subtitulo": "Conjugado",
     "exercicios": [
-      {"nome": "Agachamento Sumô (Halter)", "execucao": "4 séries • 10/15 reps", "peso": "-", "tec": "PC", "obs": "Carga pesada, foco em explosão na subida. | Pico de contração de 2s no topo (espremendo o glúteo)." },
+      {"nome": "Agachamento Sumô (pit shark)", "execucao": "4 séries • 10/15 reps", "peso": "-", "tec": "PC", "obs": "Carga pesada, foco em explosão na subida. | Pico de contração de 2s no topo (espremendo o glúteo)." },
       {"nome": "Supino Inclinado (Halteres)", "execucao": "4 séries • 8/12 reps", "peso": "-", "tec": "RP/BS", "obs": "Rest-Pause na última série. | Bi-set com Polichinelo (30s) para manter o cardio alto." },
       {"nome": "Levantamento Terra (Deadlift)", "execucao": "3 séries • 8 reps", "peso": "30", "tec": " ", "obs": " Sim | Não " },
-      {"nome": "Remada Cavalinho ou Curvada", "execucao": "3 séries • 8/15 reps", "peso": "-", "tec": "DS", "obs": " __ | Drop-set na última série (reduz o peso e faz até falhar)." },
+      {"nome": "Remada com Barra", "execucao": "3 séries • 8/15 reps", "peso": "-", "tec": "DS", "obs": " __ | Drop-set na última série (reduz o peso e faz até falhar)." },
       {"nome": "Desenvolvimento Arnold", "execucao": "3 séries • 10/12 reps", "peso": "5/7", "tec": " ", "obs": " " },
-      {"nome": "Flexão de Braços (Push-ups)", "execucao": "3 séries • até a falha", "peso": " ", "tec": " ", "obs": " "},
+      {"nome": "Levantamento Frontal (Anilha)", "execucao": "3 séries • 12 reps", "peso": " ", "tec": " ", "obs": " "},
       {"nome": "Prancha + lateral", "execucao": "3 séries • 1 min", "peso": " ", "tec": " ", "obs": " "}
     ]
   },
@@ -41,7 +41,7 @@ const dadosTreinos = {
     "subtitulo": "++Core",
     "exercicios": [
       {"nome": "Puxada Alta", "execucao": "3 séries • 12 reps", "peso": "26kg", "tec": "PC", "obs": "Segurar a barra no peito por 2s antes de subir." },
-      {"nome": "Supino Reto", "execucao": "3 séries • 12 reps", "peso": "6/6kg", "tec": " ", "obs": " ." },
+      {"nome": "Supino Reto com halteres", "execucao": "3 séries • 12 reps", "peso": "6/6kg", "tec": " ", "obs": " ." },
       {"nome": "Remada Sentada (Triângulo)", "execucao": "3 séries • 12 reps", "peso": "12.5", "tec": "RP", "obs": "Na última série: Faz 12, descansa 15s, faz mais 5." },
       {"nome": "Elevação Lateral (Ombros)", "execucao": "3 séries • 15 reps", "peso": "4", "tec": "DS", "obs": "Na última série, baixa o peso e faz mais 10 reps." },
       {"nome": "Bíceps com halter + Tríceps Corda", "execucao": "3 séries • 15/15 reps", "peso": " ", "tec": " ", "obs": " " },
