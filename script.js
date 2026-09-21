888ab2f787d7221b28fa588264fec0d637a4fbe9
Ingrid <caderno.da.ingrid@gmail.com>
2026-09-21 12:06:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,58 @@
 const treinos = {
     "1": {
-        "titulo": "Segunda-feira: Puxada (Cadeia Posterior e Postura)",
+        "titulo": "Treino A: Membros Inferiores",
         "tempo": "50-55 min",
-        "aquecimento": "5 min esteira leve + mobilidade de ombros e coluna.",
+        "aquecimento": "5 min esteira leve",
         "exercicios": [
-            "Puxada Alta Aberta: 3 x 6-8 (Foco em carga e força)",
-            "Remada Cavalinho: 3 x 10",
-            "Puxada Alta Neutra (triângulo): 3 x 10-12",
-            "Crucifixo Invertido: 3 x 12",
-            "Rosca Direta barra W: 3 x 12",
-            "Rosca Inversa: 3 x 15",
-            "Core: Elevação de Joelhos na Cadeira Capitão: 3 x 15",
-            "Cardio: 20 min de Esteira"
+            "Cadeira Extensora: 3 x 12-15",
+            "Leg Press: 3 x 10-12",
+            "Squat com halter no peito: 3 x 10",
+            "Panturrilha: 4 x 12-15"
         ]
     },
     "2": {
-        "titulo": "Terça-feira: Pernas (Dominância de Quadríceps)",
+        "titulo": "Treino B: Membros Superiores",
         "tempo": "45-55 min",
-        "aquecimento": "5 min esteira leve + mobilidade de quadril e tornozelo.",
+        "aquecimento": "5 min esteira leve",
         "exercicios": [
-            "Agachamento no Smith: 3 x 8-10 (Carga progressiva)",
-            "Agachamento Búlgaro: 3 x 10-12 cada perna",
-            "Bi-set: Cadeira Extensora + Extensão de Tornozelo em pé (Panturrilha): 3 x 12-15",
-            "Cadeira Flexora: 3 x 12",
-            "Core: Rotação de Tronco no Cabo (Polia): 3 x 12 cada lado"
+            "Supino Reto com Halteres: 3 x 10-12",
+            "AgRemada Baixa no Triângulo: 3 x 10-12",
+            "Desenvolvimento com Halteres (Sentada com banco a 80°/90°): 3 x 12",
+            "Puxada Alta Aberta no Pulley: 3 x 10-12",
+            "Tríceps Corda no Pulley: 3 x 12",
+            "Rosca Direta com Halteres (Sentada: 3 x 12",
         ]
     },
     "4": {
-        "titulo": "Quinta-feira: Empurrar (Peitoral e Ombros)",
+        "titulo": "Treino C: Membros Inferiores",
         "tempo": "45-55 min",
-        "aquecimento": "5 min esteira leve + mobilidade de ombros e escápulas.",
+        "aquecimento": "5 min esteira leve",
         "exercicios": [
-            "Supino Reto (Barra): 3 x 8-10",
-            "Desenvolvimento com Halteres: 3 x 10",
-            "Mergulho (Dips) no Gravitron: 3 x 10-12",
-            "Crucifixo Inclinado com Halteres: 3 x 12",
-            "Elevação Lateral: 4 x 12-15",
-            "Tríceps na Polia: 3 x 12",
-            "Core: Extensão Lombar no Banco Romano: 3 x 12",
-            "Cardio: 20 min esteira"
+            "Cadeira Flexora: 3 x 12",
+            "Cadeira Abdutora: 3 x 12-15",
+            "Stiff com Halteres: 3 x 10-12",
+            "Glúteo Máquina: 3 x 12-15",
+            "Panturrilha: 3 x 15"
         ]
     },
     "3": {
         "titulo": "Quarta-feira: Descanso ativo",
         "tempo": "35-40 min",
-        "aquecimento": "Mobilidade geral e alongamentos dinâmicos.",
+        "aquecimento": "5 min esteira leve",
         "exercicios": [
             "Dia de Caminhada."
         ]
     },
     "5": {
-        "titulo": "Sexta-feira: Pernas (Posterior, Lombar e Potência)",
+        "titulo": "Treino D: Membros Superiores",
         "tempo": "50-60 min",
-        "aquecimento": "5 min esteira leve + mobilidade de quadril e posterior.",
+        "aquecimento": "5 min esteira leve",
         "exercicios": [
-            "Levantamento Terra (Barra Livre): 3 x 5-6",
-            "Cadeira Flexora: 3 x 10-12",
-            "Leg Press 45º (Bilateral): 3 x 10-12",
-            "Panturrilhas Sentado: 4 x 15",
-            "Core: Abdominal Crunches: 3 x 15"
+            "Supino Inclinado com Halteres: 3 x 10-12",
+            "Remada Unilateral com Halter (Serrote): 3 x 10-12",
+            "Elevação Lateral: 3 x 12-15",
+            "Crucifixo Invertido: 3 x 12",
+            "Core: Prancha Ventral: 3 x 20-30s"
         ]
     }
 };
